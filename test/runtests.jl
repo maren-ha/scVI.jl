87e35372cacf95b4bed9256a53352e840d9c96d9
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2022-07-10 19:12:09 +0200

    aded testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,21 @@ using Test
 
 @testset "scVI.jl" begin
     # Write your tests here.
+    # PBMC 
+    @info loading data...
+    adata = load_pbmc()
+    @info data loaded, initialising object... 
+    library_log_means, library_log_vars = init_library_size(adata, 1) 
+    m = scVAE(size(adata.countmatrix,2);
+            library_log_means=library_log_means,
+            n_latent=2
+    )
+    print(summary(m))
+    training_args = TrainingArgs(
+        max_epochs=2, 
+        lr = 1e-4,
+        weight_decay=Float32(1e-6),
+    )
+    train_model!(m, adata, training_args)
+    register_latent_representation!(adata, m)
 end
