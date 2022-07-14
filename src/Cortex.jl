3a6d049953629919cb1f645c4ceb981494b57c49
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2022-07-14 14:41:07 +0200

    separate file for cortex data

---
+++ src/Cortex.jl
@@ -0,0 +1,93 @@
+#-------------------------------------------------------------------------------------
+# cortex data 
+#-------------------------------------------------------------------------------------
+
+function load_cortex_from_h5ad(anndata::HDF5.File)
+    countmatrix = read(anndata, "layers")["counts"]' # shape: cell x gene 
+    summary_stats = read(anndata, "uns")["_scvi"]["summary_stats"]
+    layers = read(anndata, "layers")
+    obs = read(anndata, "obs")
+    data_registry = read(anndata, "uns")["_scvi"]["data_registry"]
+    celltype_numbers = read(anndata, "obs")["cell_type"] .+1 # for Julia-Python index conversion
+    celltype_categories = read(anndata, "obs")["__categories"]["cell_type"]
+    celltypes = celltype_categories[celltype_numbers]
+    return Matrix(countmatrix), layers, obs, summary_stats, data_registry, celltypes
+end
+
+# assumes Python adata object 
+function init_cortex_from_h5ad(filename::String=joinpath(@__DIR__, "../data/cortex_anndata.h5ad"))
+    anndata = open_h5_data(filename)
+    countmatrix, layers, obs, summary_stats, data_registry, celltypes = load_cortex_from_h5ad(anndata)
+    ncells, ngenes = size(countmatrix)
+    adata = AnnData(
+        countmatrix=countmatrix,
+        ncells=ncells,
+        ngenes=ngenes,
+        layers=layers,
+        obs=obs,
+        summary_stats=summary_stats,
+        registry=data_registry,
+        celltypes=celltypes
+    )
+    return adata
+end
+
+function init_cortex_from_url(save_path::String=joinpath(@__DIR__, "../data/"))
+
+    url = "https://storage.googleapis.com/linnarsson-lab-www-blobs/blobs/cortex/expression_mRNA_17-Aug-2014.txt"
+    path_to_file = joinpath(save_path, "expression.bin")
+    if !isfile(path_to_file)
+        download(url, path_to_file)
+    end
+    csvfile = DelimitedFiles.readdlm(path_to_file, '\t')
+    precise_clusters = csvfile[2,3:end]
+    clusters = csvfile[9,3:end]
+    gene_names = String.(csvfile[12:end,1])
+
+    countmatrix = Float32.(csvfile[12:end,3:end]')
+
+    labels = fill(0, length(clusters))
+    for i in 1:length(unique(clusters))
+        labels[findall(x -> x == unique(clusters)[i], clusters)] .= i
+    end
+
+    cellinfos = Dict(
+        "cell_type" => clusters,
+        "labels" => labels,
+        "precise_labels" => precise_clusters,
+        "tissue" => String.(csvfile[1,3:end]),
+        "group" => Int.(csvfile[2,3:end]),
+        "totalmRNA" => Int.(csvfile[3,3:end]),
+        "well" => Int.(csvfile[4,3:end]),
+        "sex" => Int.(csvfile[5,3:end]),
+        "age" => Int.(csvfile[6,3:end]),
+        "diameter" => Float32.(csvfile[7,3:end]),
+        "cell_id" => String.(csvfile[8,3:end])
+    )
+
+    geneinfos = Dict(
+        "gene_names" => gene_names
+    )
+
+    @assert size(countmatrix,1) == length(clusters)
+    @assert size(countmatrix,2) == length(gene_names)
+
+    adata = AnnData(
+        countmatrix = countmatrix,
+        ncells = size(countmatrix,1),
+        ngenes = size(countmatrix,2),
+        obs = cellinfos, 
+        vars = geneinfos, 
+        celltypes = cellinfos["cell_type"]
+    )
+    return adata
+end
+
+function load_cortex(path::String=joinpath(@__DIR__, "../data/"))
+    if isfile(string(path, "cortex_anndata.h5ad"))
+        adata = init_cortex_from_h5ad(string(path, "cortex_anndata.h5ad"))
+    else
+        adata = init_cortex_from_url(path)
+    end
+    return adata 
+end
