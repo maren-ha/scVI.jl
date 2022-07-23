379fcdcd2961b9ce0a9dfbfa9f420e401357a381
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2022-07-23 12:03:59 +0200

    added docstring

--- src/PBMC.jl
+++ src/PBMC.jl
@@ -1,7 +1,32 @@
 #-------------------------------------------------------------------------------------
 # pbmc data from csv 
 #-------------------------------------------------------------------------------------
+"""
+    load_pbmc(path::String = joinpath(@__DIR__, "../data/"))
 
+Loads build-in `pbmc` dataset from [Zheng et al. 2017](https://www.nature.com/articles/ncomms14049). 
+Specifically, the PBMC8k version is used and has been preprocessed according to the [Bioconductor workflow](https://bioconductor.org/books/3.15/OSCA.workflows/unfiltered-human-pbmcs-10x-genomics.html).
+
+The function loads the following files from the folder passed as `path` (default: `data` subfolder of scVI repo.)
+ - `PBMC_counts.csv`: countmatrix  
+ - `PBMC_annotation.csv`: cell type annotation
+
+These files can be downloaded from the repo using [Git LFS](https://git-lfs.github.com) and running `git-lfs checkout`
+after cloning the repository. 
+
+From these input files, a Julia `AnnData` object is created. The countmatrix contains information on 
+cell barcodes and gene names. The gene name and celltype information is stored in the `vars` and `obs` 
+dictionaries of the `AnnData` object, respectively. 
+
+Returns the Julia `AnnData` object.
+
+**Example** 
+---------------------------
+    julia> load_pbmc()
+        AnnData object with a countmatrix with 7480 cells and 200 genes
+        unique celltypes: ["B-cells", "CD4+ T-cells", "Monocytes", "CD8+ T-cells", "NK cells", "NA", "HSC", "Erythrocytes"]
+        training status: not trained
+"""
 function load_pbmc(path::String = joinpath(@__DIR__, "../data/"))
     counts = CSV.read(string(path, "PBMC_counts.csv"), DataFrame)
     celltypes = vec(string.(CSV.read(string(path, "PBMC_annotation.csv"), DataFrame)[:,:x]))
