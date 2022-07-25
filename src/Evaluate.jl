a82feee5d41b4508f60d208893ec4f6a210148a6
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2022-07-25 23:34:21 +0200

    add missing docstrings

--- src/Evaluate.jl
+++ src/Evaluate.jl
@@ -1,5 +1,11 @@
 """
     register_latent_representation!(adata::AnnData, m::scVAE)
+
+Calculates the latent representation obtained from encoding the `countmatrix` of the `AnnData` object 
+with a trained `scVAE` model by applying the function `get_latent_representation(m, adata.countmatrix)`. 
+Stored the latent representation in the `scVI_latent` field of the input `AnnData` object. 
+
+Returns the modified `AnnData` object.
 """
 function register_latent_representation!(adata::AnnData, m::scVAE)
     adata.scVI_latent = get_latent_representation(m, adata.countmatrix)
@@ -9,6 +15,15 @@ end
 
 """
     register_umap_on_latent!(adata::AnnData, m::scVAE)
+
+Calculates a UMAP (Uniform Manifold Projection and Embedding, [McInnes et al. 2018](https://arxiv.org/abs/1802.03426)) embedding of the latent representation obtained from encoding the `countmatrix` of the `AnnData` object 
+with a trained `scVAE` model. If a latent representation is already stored in `adata.scVI_latent`, this is used for calculating 
+the UMAP, if not, a latent representation is calculated and registered by calling `register_latent_representation!(adata, m)`. 
+
+The UMAP is calculated using the Julia package [UMAP.jl](https://github.com/dillondaudert/UMAP.jl) with default parameters. 
+It is then stored in the `scVI_latent_umap` field of the input `AnnData` object. 
+
+Returns the modified `AnnData` object.    
 """
 function register_umap_on_latent!(adata::AnnData, m::scVAE)
     if isnothing(adata.scVI_latent)
@@ -21,9 +36,39 @@ function register_umap_on_latent!(adata::AnnData, m::scVAE)
 end
 
 """
-    plot_umap_on_latent(m::scVAE, adata::AnnData; save_plot::Bool=false, seed::Int=987, filename::String="UMAP_on_latent.pdf")
+    function plot_umap_on_latent(
+        m::scVAE, adata::AnnData; 
+        save_plot::Bool=false, 
+        seed::Int=987, 
+        filename::String="UMAP_on_latent.pdf"
+    )
+
+Plots a UMAP embedding of the latent representation obtained from encoding the countmatrix of the `AnnData` object with the `scVAE` model. 
+If no UMAP representation is stored in `adata.scVI_latent_umap`, it is calculated and registered by calling `register_umap_on_latent(adata, m)`.
+
+By default, the cells are color-coded according to the `celltypes` field of the `AnnData` object. 
+
+!TODO: add fallback for missing celltype annotation (`adata.celltypes = nothing`)
+
+For plotting, the [VegaLite.jl](https://www.queryverse.org/VegaLite.jl/stable/) package is used.
+
+**Arguments:**
+---------------
+ - `m::scVAE`: trained `scVAE` model to use for embedding the data with the model encoder
+ - `adata:AnnData`: data to embed with the model; `adata.countmatrix` is encoded with `m`
+
+ **Keyword arguments:**
+ -------------------
+ - `save_plot::Bool=true`: whether or not to save the plot
+ - `filename::String="UMAP_on_latent.pdf`: filename under which to save the plot. Has no effect if `save_plot==false`.
+ - `seed::Int=987`: which random seed to use for calculating UMAP (to ensure reproducibility)
 """
-function plot_umap_on_latent(m::scVAE, adata::AnnData; save_plot::Bool=false, seed::Int=987, filename::String="UMAP_on_latent.pdf")
+function plot_umap_on_latent(
+    m::scVAE, adata::AnnData; 
+    save_plot::Bool=false, 
+    filename::String="UMAP_on_latent.pdf",
+    seed::Int=987
+    )
 
     if isnothing(adata.scVI_latent) 
         @info "no latent representation saved in AnnData object, calculating based on scVAE model..."
@@ -61,9 +106,38 @@ function prcomps(mat, standardizeinput = true)
 end
 
 """
-    plot_pca_on_latent(m::scVAE, adata::AnnData; save_plot::Bool=false, filename::String="PCA_on_latent.pdf")
+    plot_pca_on_latent(
+        m::scVAE, adata::AnnData; 
+        save_plot::Bool=false, 
+        filename::String="PCA_on_latent.pdf"
+    )
+    
+Plots a PCA embedding of the latent representation obtained from encoding the countmatrix of the `AnnData` object with the `scVAE` model. 
+If no latent representation is stored in `adata.scVI_latent`, it is calculated and registered by calling `register_latent_representation(adata, m)`.
+
+PCA is calculated using the singular value decomposition implementation in [`LinearAlgebra.jl`](https://docs.julialang.org/en/v1/stdlib/LinearAlgebra/#LinearAlgebra.svd), see `?LinearAlgebra.svd`. For details on the PCA implementation, see the source code in the `prcomps` function in `src/Evaluate.jl`.
+
+By default, the cells are color-coded according to the `celltypes` field of the `AnnData` object. 
+
+!TODO: add fallback for missing celltype annotation (`adata.celltypes = nothing`)
+
+For plotting, the [VegaLite.jl](https://www.queryverse.org/VegaLite.jl/stable/) package is used.
+
+**Arguments:**
+---------------
+- `m::scVAE`: trained `scVAE` model to use for embedding the data with the model encoder
+- `adata:AnnData`: data to embed with the model; `adata.countmatrix` is encoded with `m`
+
+**Keyword arguments:**
+-------------------
+- `save_plot::Bool=true`: whether or not to save the plot
+- `filename::String="UMAP_on_latent.pdf`: filename under which to save the plot. Has no effect if `save_plot==false`.
 """
-function plot_pca_on_latent(m::scVAE, adata::AnnData; save_plot::Bool=false, filename::String="PCA_on_latent.pdf")
+function plot_pca_on_latent(
+    m::scVAE, adata::AnnData; 
+    save_plot::Bool=false, 
+    filename::String="PCA_on_latent.pdf"
+    )
 
     if isnothing(adata.scVI_latent)
         @info "no latent representation saved in AnnData object, calculating based on scVAE model..."
