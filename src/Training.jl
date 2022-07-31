888bcab69c73ee8370b87534240a1448b4b12add
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2022-07-31 19:38:02 +0200

    added train_supervised_model!

--- src/Training.jl
+++ src/Training.jl
@@ -37,7 +37,7 @@ Flux.params(m::scVAE) = Flux.params(m.z_encoder, m.l_encoder, m.decoder)
 """
     train_model!(m::scVAE, adata::AnnData, training_args::TrainingArgs)
 
-Trains an `scVAE` model on an `AnnData` object, behavior controlled by a `TrainingArgs` object: 
+Trains an `scVAE` model on an `AnnData` object, where the behaviour is controlled by a `TrainingArgs` object: 
 Defines the ADAM SGD optimiser, collects the model parameters, optionally splits data in training and testdata and 
 initialises a `Flux.DataLoader` storing the data in the countmatrix of the `AnnData` object in batches. 
 Updates the model parameters via stochastic gradient for the specified number of epochs, 
@@ -83,4 +83,66 @@ function train_model!(m::scVAE, adata::AnnData, training_args::TrainingArgs)
     @info "training complete!"
     adata.is_trained = true
     return m, adata
+end
+
+"""
+    train_supervised_model!(m::scVAE, adata::AnnData, labels::AbstractVecOrMat{S}, training_args::TrainingArgs) where S <: Real
+
+Trains a `scVAE` model on an `AnnData` object, where the latent representation is additionally trained in a supervised way to match the provided `labels`, 
+where the behaviour is controlled by a `TrainingArgs` object: 
+
+Defines the ADAM SGD optimiser, collects the model parameters, optionally splits data in training and testdata and 
+initialises a `Flux.DataLoader` storing the data in the countmatrix of the `AnnData` object
+and the corresponding `labels` for the supervision of the latent representation in batches. 
+
+The loss function used is the ELBO with an additional supervised term (can be checked in the function `supervised_loss` in `src/ModelFunctions.jl`: 
+In addition to the `scVAE` model and the count data, it has as additional input the provided `labels`, that need to have the same dimension as the latent represenation. 
+The mean squared error between the latent representation 
+and the labels is calculated and added to the standard ELBO loss. 
+
+Updates the model parameters via stochastic gradient for the specified number of epochs, 
+optionally prints out progress and current loss values. 
+
+Returns the trained `scVAE` model.
+"""
+function train_supervised_model!(m::scVAE, adata::AnnData, labels::AbstractVecOrMat{S}, training_args::TrainingArgs) where S <: Real
+
+    @assert size(labels) == (adata.ncells, m.n_latent)
+
+    opt = Flux.Optimiser(Flux.Optimise.WeightDecay(training_args.weight_decay), ADAM(training_args.lr))
+    ps = Flux.params(m)
+
+    if training_args.train_test_split
+        trainsize = training_args.trainsize
+        validationsize = 1 - trainsize # nothing 
+        train_inds = shuffle!(collect(1:adata.ncells))[1:Int(ceil(trainsize*adata.ncells))]
+    else
+        train_inds = collect(1:adata.ncells);
+    end
+
+    dataloader = Flux.DataLoader((adata.countmatrix[train_inds,:]', labels[train_inds,:]'), batchsize=training_args.batchsize, shuffle=true)
+
+    train_steps=0
+    @info "Starting training for $(training_args.max_epochs) epochs..."
+    if training_args.progress
+        progress = Progress(length(dataloader)*training_args.max_epochs);
+    end
+
+    for epoch in 1:training_args.max_epochs
+        kl_weight = get_kl_weight(training_args.n_epochs_kl_warmup, training_args.n_steps_kl_warmup, epoch, 0)
+        for d in dataloader
+            curloss, back = Flux.pullback(ps) do 
+                supervised_loss(m, d...; kl_weight=kl_weight)    
+            end
+            grad = back(1f0)
+            Flux.Optimise.update!(opt, ps, grad)
+            if training_args.progress
+                next!(progress; showvalues=[(:loss, curloss)]) 
+            elseif (train_steps % training_args.verbose_freq == 0) && training_args.verbose
+                @info "epoch $(epoch):" loss=curloss
+            end
+            train_steps += 1
+        end
+    end
+    return m
 end
