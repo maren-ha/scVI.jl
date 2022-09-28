0ed6b504dcef634c9ffd10d08fb6f0768ba7e47e
Sara Al-Rawi <alrawi@imbi.uni-freiburg.de>
2022-09-28 13:33:35 +0200

    training function for multimodality improved

--- src/Training.jl
+++ src/Training.jl
@@ -1,22 +1,26 @@
 Base.@kwdef mutable struct TrainingArgs
     trainsize::Float32 = 0.9f0
     train_test_split::Bool=false
-    batchsize::Int=64
-    max_epochs::Int = 20
+    batchsize::Int=128
+    max_epochs::Int = 50
     lr::Float64 = 1e-3
     weight_decay::Float32 = 0.0f0
     n_steps_kl_warmup = nothing
     n_epochs_kl_warmup::Int=1
     progress::Bool=false
     verbose::Bool=true
-    verbose_freq::Int=0
-    log_path::String
+    infotime::Int=1
+    tblogger::Bool=true
+    savepath::String=nothing
+    use_cuda::Bool=false
+    seed::Int=123
+    checktime::Int=5
 end
 
-function start_training(m,adata::AbstractArray{AnnData}, training_args::TrainingArgs,logger)
+function start_training!(m,adata::AbstractArray{AnnData}, training_args::TrainingArgs,logger)
     # values to be returned m , adata 
-    m , adata = train_model!(typeof(m),m, adata,training_args,logger)
-    return m, adata 
+    m , adata, losses = train_model!(typeof(m),m, adata,training_args,logger)
+    return m, adata, losses
 end
 
 Flux.params(m::scVAE) = Flux.params(m.z_encoder, m.l_encoder, m.decoder)
@@ -24,8 +28,6 @@ function train_model!(::Type{scVAE},m, adata::AbstractArray{AnnData}, training_a
     opt = Flux.Optimiser(Flux.Optimise.WeightDecay(training_args.weight_decay), ADAM(training_args.lr))
     ps = Flux.params(m)
 
-    # unpack the data 
-    adata = adata[1]
     if training_args.train_test_split
         trainsize = training_args.trainsize
         validationsize = 1 - trainsize # nothing 
@@ -57,9 +59,7 @@ function train_model!(::Type{scVAE},m, adata::AbstractArray{AnnData}, training_a
                 
                 end
             end
-            train_steps += 1
-            # calling garbage collectore
-            
+            train_steps += 1  
         end
     end
     @info "training complete!"
@@ -68,86 +68,20 @@ function train_model!(::Type{scVAE},m, adata::AbstractArray{AnnData}, training_a
 end
 
 ##########################################################################################################################
-Flux.params(m::scMultiVAE_) = Flux.params( m.z_encoder[1],m.z_encoder[2], m.l_encoder, m.decoder[1], m.l_encoder_, m.decoder[2])
-
-
-function train_model!(::Type{scMultiVAE_},m,adata::AbstractArray{AnnData}, training_args::TrainingArgs,logger)
-
-    opt = Flux.Optimiser(Flux.Optimise.WeightDecay(training_args.weight_decay), ADAM(training_args.lr))
-    ps = Flux.params(m)
-    # unpack the data 
-    adata1 , adata2 = adata[1], adata[2]
-    if training_args.train_test_split
-        trainsize = training_args.trainsize
-        validationsize = 1 - trainsize # nothing 
-        train_inds = shuffle!(collect(1:adata1.ncells))[1:Int(ceil(trainsize*adata1.ncells))]
-    else
-        train_inds_rna = collect(1:adata1.ncells);
-        train_inds_protein = collect(1:adata2.ncells);
-    end
-
-    dataloader_mod1 = Flux.DataLoader(adata1.countmatrix[train_inds_rna,:]', batchsize=training_args.batchsize, shuffle=true)
-    dataloader_mod2 = Flux.DataLoader(adata2.countmatrix[train_inds_protein,:]', batchsize=training_args.batchsize, shuffle=true)
-
-    train_steps=0
-    @info "Starting training for $(training_args.max_epochs) epochs..."
-    progress = training_args.progress && Progress(length(dataloader_mod1)*training_args.max_epochs);
-    for epoch in 1:training_args.max_epochs
-        kl_weight = get_kl_weight(training_args.n_epochs_kl_warmup, training_args.n_steps_kl_warmup, epoch, 0)
-        for (mod1,mod2) in zip(dataloader_mod1,dataloader_mod2)
-            curloss, back = Flux.pullback(ps) do 
-                loss(m, [Float32.(mod1),Float32.(mod2)], kl_weight=kl_weight)   
-            end
-            grad = back(1f0)
-            Flux.Optimise.update!(opt, ps, grad)
-            if training_args.progress
-                next!(progress; showvalues=[(:loss, curloss)])
-                # this call back for our sanity check ... 
-                total, recon_rna , recon_pro = objective_multimodal_callback(m, mod1, mod2, kl_weight=kl_weight)
-                @info "epoch $(epoch):" recon_rna=recon_rna
-                @info "epoch $(epoch):" recon_pro=recon_pro
-                with_logger(logger) do
-                    @info string("Training_Loss") Total_loss=curloss
-                    @info string("RNA_Loss") RNA_loss=recon_rna
-                    @info string("Protein_Loss") Protein_loss=recon_pro
-                end
-            elseif (train_steps % training_args.verbose_freq == 0) && training_args.verbose
-                total, recon_rna , recon_pro   = objective_multimodal_callback(m, mod1, mod2, kl_weight=kl_weight)
-                @info "epoch $(epoch):" total=total
-                @info "epoch $(epoch):" recon_rna=recon_rna
-                @info "epoch $(epoch):" recon_pro=recon_pro
-                with_logger(logger) do
-                    @info string("Training_Loss") Total_loss=curloss
-                    @info string("RNA_Loss") RNA_loss=recon_rna
-                    @info string("Protein_Loss") Protein_loss=recon_pro
-                end
-            end
-            train_steps += 1
-        end
-
-    end
-    @info "training complete!"
-    adata1.is_trained = true
-    adata2.is_trained = true
-    adata = [adata1, adata2]
-    return m, adata
-end
-
+Flux.params(m::scMultiVAE_) = Flux.params(m.z_encoder[1],m.z_encoder[2], m.l_encoder, m.decoder[1], m.l_encoder_, m.decoder[2])
 
 function objective_multimodal_callback(m::scMultiVAE_, mod1::AbstractMatrix{S}, mod2::AbstractMatrix{S}; kl_weight::Float32=1.0f0) where S <: Real 
     #apply inference on multimodality
-    x = [mod1, mod2] 
+    x = [Float32.(mod1),Float32.(mod2)]
+    mod1, mod2 = Float32.(mod1),Float32.(mod2)
     z_rna ,z_protein, qz_m_rna, qz_v_rna,qz_m_p, qz_v_p, ql_m, ql_v, library  = do_inference(m, x)
     z = [z_rna, z_protein]
     #apply generative on multimodal
     px_scale, px_r, px_rate, px_dropout, px_scale_, px_r_, px_rate_, px_dropout_ = do_generative(m,z,library)
-        
     # no off diagonal calculation
-    kl_divergence_rna = -0.5f0 .* sum(1.0f0 .+ log.(qz_v_rna) - qz_m_rna.^2 .- qz_v_rna, dims=1) # batchxlatent_dimentionality
-
+    kl_divergence_rna = -0.5f0 .* sum(1.0f0 .+ log.(qz_v_rna) - qz_m_rna.^2 .- qz_v_rna, dims=1) # batchxlatent
     kl_divergence_p = -0.5f0 .* sum(1.0f0 .+ log.(qz_v_p) - qz_m_p.^2 .- qz_v_p, dims=1)
     # compute reconstruction error for rna modality 
-    
     reconstruction_loss_rna =  sum(-log_zinb_positive(Float32.(mod1), Float32.(px_rate), Float32.(px_r), Float32.(px_dropout)), dims=1)
     # compute reconstruction error for protein modality 
     # px_z
@@ -155,6 +89,126 @@ function objective_multimodal_callback(m::scMultiVAE_, mod1::AbstractMatrix{S}, 
     # total reconstraction loss 
     recon_loss = sum(reconstruction_loss_p) + sum(reconstruction_loss_rna)
     kl_loss = sum(kl_divergence_rna) + sum(kl_divergence_p)
-    multimodal_total_loss = sum(0.5 * (recon_loss + (kl_weight .* kl_loss)))
-    return mean(multimodal_total_loss) , mean(reconstruction_loss_rna), mean(reconstruction_loss_p)
+    multimodal_total_loss = sum(0.5 * (recon_loss - (kl_weight .* kl_loss)))    
+    return (moe_loss=mean(multimodal_total_loss) |> round4 , loss_rna=mean(reconstruction_loss_rna) |> round4, loss_protein=mean(reconstruction_loss_p) |> round4)
+end
+
+## Utility functions
+num_params(model) = sum(length, Flux.params(model))
+round4(x) = round(x, digits=4)
+function train_model!(::Type{scMultiVAE_},m,multi_adata::AbstractArray{AnnData}, training_args::TrainingArgs,logger)
+    args = training_args
+    # create seed ....
+    args.seed > 0 && Random.seed!(args.seed)
+    use_cuda = args.use_cuda && CUDA.functional()
+    
+    if use_cuda
+        device = gpu
+        @info "Training on GPU"
+    else
+        device = cpu
+        @info "Training on CPU"
+    end
+
+    ## Get the data ... 
+    multi_adata = multi_adata[1]
+
+    mod1 = multi_adata.countmatrix[:,1:4000]
+    mod2 = multi_adata.countmatrix[:,4001:4134]
+    if  args.train_test_split
+        trainsize = args.trainsize
+        validationsize = 1 - trainsize # nothing 
+        train_inds = shuffle!(collect(1: multi_adata.ncells))[1:Int(ceil(trainsize* multi_adata.ncells))]
+        # train loaders 
+        dataloader_mod1 = Flux.DataLoader(mod1[train_inds,:]', batchsize=training_args.batchsize)
+        dataloader_mod2 = Flux.DataLoader(mod2[train_inds,:]', batchsize=training_args.batchsize)
+        # Get non intersection indices set = validation indices       
+        val_inds = collect(1: multi_adata.ncells)[collect(1: multi_adata.ncells) .∉ Ref(train_inds)]
+        val_dataloader_mod1 = Flux.DataLoader(mod1[val_inds,:]', batchsize=training_args.batchsize)
+        val_dataloader_mod2 = Flux.DataLoader(mod2[val_inds,:]', batchsize=training_args.batchsize)        
+    else
+        train_inds_rna = collect(1: multi_adata.ncells);
+        train_inds_protein = collect(1: multi_adata.ncells);
+        # get dataloaders for each modality ... 
+        dataloader_mod1 = Flux.DataLoader(mod1[train_inds_rna,:]', batchsize=training_args.batchsize,shuffle=false,partial=false)
+        dataloader_mod2 = Flux.DataLoader(mod2[train_inds_protein,:]', batchsize=training_args.batchsize,shuffle=false,partial=false)
+    end
+
+    ## move the model to a device ...
+    model = m|> device
+
+    @info "The MulitScVI has: $(num_params(model)) trainable params"    
+    opt = Flux.Optimiser(Flux.Optimise.WeightDecay(args.weight_decay), ADAM(args.lr))
+    
+    ps = Flux.params(model)  
+    
+    moes_loss = []
+    loss_rnas = []
+    loss_proteins = []
+
+    val_moes_loss = []
+    val_loss_rnas = []
+    val_loss_proteins = []
+
+    ## Report losses ...
+    function report(epoch,kl_weight)
+        train = objective_multimodal_callback(model, mod1[train_inds_rna,:]', mod2[train_inds_protein,:]', kl_weight=kl_weight)
+        push!(moes_loss,train.moe_loss)
+        push!(loss_rnas,train.loss_rna)
+        push!(loss_proteins,train.loss_protein)
+        println("Epoch: $epoch   Train: $(train)")
+        if args.tblogger
+            set_step!(logger, epoch)
+            with_logger(logger) do
+                @info "train" moe_loss=train.moe_loss 
+                @info "train_RNA" loss_rna=train.loss_rna  
+                @info "train_Protein" loss_protein=train.loss_protein 
+            end
+
+        end
+    end
+    
+    ## START TRAINING
+    @info "Starting training for $(training_args.max_epochs) epochs..."
+    progress = args.progress && Progress(length(dataloader_mod1)*args.max_epochs);
+    report(0,0.0f0)
+    for epoch in 1:args.max_epochs
+        kl_weight = get_kl_weight(training_args.n_epochs_kl_warmup, training_args.n_steps_kl_warmup, epoch, 0)
+        for (mod1_, mod2_) in zip(dataloader_mod1,dataloader_mod2)
+            mod1_, mod2_ = mod1_ |> device, mod2_ |> device
+            curloss, back = Flux.pullback(ps) do 
+                loss(m, [Float32.(mod1_),Float32.(mod2_)], kl_weight=kl_weight)    
+            end
+            grad = back(1f0)
+            if args.progress
+                next!(progress; showvalues=[(:loss, curloss)])  
+            end
+
+            Flux.Optimise.update!(opt, ps, grad)
+        end
+        ## Printing and logging
+        epoch % args.infotime == 0 && report(epoch,kl_weight)
+        if args.checktime > 0 && epoch % args.checktime == 0
+            
+            !ispath(args.savepath) && mkpath(args.savepath)            
+            if args.train_test_split
+                validation = objective_multimodal_callback(model, mod1[val_inds,:]', mod2[val_inds,:]', kl_weight=kl_weight)
+                push!(val_moes_loss,validation.moe_loss)
+                push!(val_loss_rnas,validation.loss_rna)
+                push!(val_loss_proteins,validation.loss_protein)
+            end
+            modelpath = joinpath(args.savepath, "model.bson") 
+            let model = cpu(model) ## return model to cpu before serialization
+                BSON.@save modelpath model epoch
+            end
+            @info "Model saved in \"$(modelpath)\""
+        end
+            
+    end
+    @info "training complete!"
+    multi_adata.is_trained = true
+    
+    return model, multi_adata, 
+    (moes_train=moes_loss, mod1_train=loss_rnas, mod2_train=loss_proteins, 
+    moes_val=val_moes_loss, mod1_val=val_loss_rnas, mod2_val2=val_loss_proteins)
 end
