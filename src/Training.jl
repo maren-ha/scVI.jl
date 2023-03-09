a6dcd9e11a263259749b61ceceee32bd8053abee
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2023-03-09 11:21:11 +0100

    fix warning about n_batch and length of library_log_means in training function

--- src/Training.jl
+++ src/Training.jl
@@ -71,7 +71,7 @@ function setup_batch_indices_for_library_scaling(m::scVAE, adata::AnnData, batch
         end
     elseif m.use_observed_lib_size
         if m.n_batch > 1 || (!isnothing(m.library_log_means) && length(m.library_log_means) > 1)
-            @warn "m.n_batch = $(m.n_batch) > 1, but observed library size is used, thus ignoring potential batch effects"
+            @warn "either m.n_batch > 1 or length of observed library_log_means/vars vector > 1, but observed library size is used, thus ignoring potential batch effects"
         end
         verbose && @info "Using observed library size in each training batch, thus ignoring potential experimental batch effects"
        @assert batch_indices == ones(Int64, size(adata.countmatrix,1))
