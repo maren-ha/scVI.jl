f61c2685e820cdcec13adf0b18d0ac3e901a9934
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2023-03-10 02:00:10 +0100

    use proper submodules

--- src/scVI.jl
+++ src/scVI.jl
@@ -1,11 +1,20 @@
 module scVI
 
 # for data handling
-using CSV
-using DataFrames
-using HDF5
-using DelimitedFiles
-using JLD2
+include("data/DataProcessing.jl")
+using .DataProcessing
+
+export
+    AnnData,
+    subset_adata, subset_adata!,
+    read_h5ad, write_h5ad,
+    init_library_size,
+    highly_variable_genes, highly_variable_genes!, subset_to_hvg!,
+    estimate_size_factors, normalize_size_factors, normalize_size_factors!, 
+    load_cortex_from_h5ad, load_cortex_from_url, load_cortex, 
+    load_pbmc, 
+    load_tasic, subset_tasic!
+#
 
 # core package functionality 
 using Distributions
@@ -16,23 +25,11 @@ using ProgressMeter
 using SpecialFunctions # for loggamma
 using StatsBase
 
-# scvis 
-using Distances
-using SliceMap
-
 # evaluation: UMAP, PCA and plots  
 using LinearAlgebra
 using UMAP 
 using VegaLite
 
-include("data/AnnData.jl")
-include("data/FileIO.jl")
-include("data/HighlyVariableGenes.jl")
-include("data/LibrarySizeNormalization.jl")
-include("data/Transformations.jl")
-include("data/Cortex.jl")
-include("data/PBMC.jl")
-include("data/Tasic.jl")
 include("Utils.jl")
 include("EncoderDecoder.jl")
 include("scVAEmodel.jl")
@@ -41,21 +38,8 @@ include("CountDistributions.jl")
 include("ModelFunctions.jl")
 include("Training.jl")
 include("Evaluate.jl")
-include("scvis/scvis.jl")
-include("scvis/scvis_diff.jl")
-include("scvis/scvis_train.jl")
 
-# just a random comment 
-export 
-    AnnData,
-    subset_adata, subset_adata!,
-    read_h5ad, write_h5ad,
-    init_library_size,
-    highly_variable_genes, highly_variable_genes!, subset_to_hvg!,
-    estimate_size_factors, normalize_size_factors, normalize_size_factors!, 
-    load_cortex_from_h5ad, load_cortex_from_url, load_cortex, 
-    load_pbmc, 
-    load_tasic, subset_tasic!,
+export
     scVAE, scEncoder, scDecoder, scLinearDecoder, scLDVAE,
     TrainingArgs, 
     train_model!, train_supervised_model!,
@@ -63,9 +47,16 @@ export
     register_latent_representation!, register_umap_on_latent!,
     plot_umap_on_latent, 
     plot_pca_on_latent,
-    sample_from_prior, sample_from_posterior,
+    sample_from_prior, sample_from_posterior
+#
+
+# scvis 
+include("scvis/scvis.jl")
+using .scvis
+
+export
     train_scvis_model!, 
     compute_transition_probs, compute_differentiable_transition_probs,
     tsne_repel, scvis_loss, tsne_loss
-# 
+#
 end
