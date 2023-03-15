8b29bf7f75ca12b313331dd74950557651234811
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2023-03-15 13:59:46 +0100

    added fast version of Gaussian generative distribution

--- src/OtherDistributions.jl
+++ src/OtherDistributions.jl
@@ -1,7 +1,13 @@
 # additional support for Gaussian and Bernoulli generative distributions 
 
-function log_normal(x::AbstractMatrix{S}, mu::AbstractMatrix{S}, logsigma::AbstractVecOrMat{S}) where S <: Real
-    return logpdf.(Normal.(mu, sqrt.(exp.(logsigma))), x)
+# slow version 
+#function log_normal(x::AbstractMatrix{S}, mu::AbstractMatrix{S}, logsigma::AbstractVecOrMat{S}) where S <: Real
+#    return logpdf.(Normal.(mu, sqrt.(exp.(logsigma))), x)
+#end
+
+function log_normal(x::AbstractMatrix{S}, μ::AbstractMatrix{S}, logσ::AbstractVecOrMat{S}) where S <: Real
+    res = @fastmath (-(x .- μ).^2 ./ (2.0f0 .* exp.(logσ))) .- 0.5f0 .* (log(S(2π)) .+ logσ)
+    return res
 end
 
 function log_binary(x::AbstractMatrix{S}, dec_z::AbstractMatrix{S}, eps::S=S(1e-8)) where S <: Real
