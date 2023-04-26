66f5581e4be94479d2b9267993952f3f5d5633ac
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2023-04-27 00:14:14 +0200

    fix loading from file

--- src/data/PBMC.jl
+++ src/data/PBMC.jl
@@ -47,13 +47,13 @@ function load_pbmc(path::String = "data/")
                     layers = Dict("counts" => counts)
         )
     else
-        filename_jld2 = joinpath(path, "pbmc.jld2")
-        filename_in_scvi = string(dirname(pathof(scVI)),"/../data/pbmc.jld2")
+        filename_h5ad = joinpath(path, "pbmc.h5ad")
+        filename_in_scvi = string(dirname(pathof(scVI)),"/../data/pbmc.h5ad")
         # saved via save(filename_jld2, Dict("adata_pbmc" => adata)); save(filename_in_scvi, Dict("adata_pbmc" => adata))
-        if isfile(filename_jld2)
-            adata = jldopen(filename_jld2)["adata_pbmc"]
+        if isfile(filename_h5ad)
+            adata = readh5ad(filename_h5ad)
         elseif isfile(filename_in_scvi)
-            adata = jldopen(filename_in_scvi)["adata_pbmc"]
+            adata = readh5ad(filename_in_scvi)
         end
     end
     return adata
