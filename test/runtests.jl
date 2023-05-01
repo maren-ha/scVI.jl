39359446556d55d53ce059da221d1bee5a2b0024
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2023-05-01 22:07:14 +0200

    extend runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using scVI
 using Test
 using DataFrames
+using Random
 
-@testset "Preprocessing" begin
+@testset "Preprocessing with Cortex data" begin
     
     using scVI
     @info "loading data..."
@@ -43,10 +44,14 @@ using DataFrames
 
     # check HVG selection
     @info "testing HVG selection..."
-    hvgdict = highly_variable_genes(adata, n_top_genes=1200)
-    @test sum(hvgdict[!,"highly_variable"]) == 1200
-    highly_variable_genes!(adata, n_top_genes=1200)
-    @test sum(adata.var[!,"highly_variable"]) == 1200
+    hvgdf = highly_variable_genes(adata, n_top_genes=1200)
+    @test sum(hvgdf[!,"highly_variable"]) == 1200
+    hvgdf_batch = highly_variable_genes(adata, n_top_genes = 500, batch_key = "sex")
+    @test hasproperty(hvgdf_batch, :highly_variable_nbatches)
+    adata.var = hvgdf_batch
+    @test sum(adata.var.highly_variable) == 500
+    highly_variable_genes!(adata, n_top_genes=1200, replace_hvgs = true)
+    @test sum(adata.var.highly_variable) == 1200
     subset_to_hvg!(adata, n_top_genes=1200)
     @test nrow(adata.var) == 1200
 
@@ -58,7 +63,7 @@ using DataFrames
     @test haskey(adata.obsm, "umap")
 end
 
-@testset "PBMC.jl" begin
+@testset "PBMC data" begin
     # PBMC
     @info "testing PBMC data loading + VAE model initialization..."
     using scVI
@@ -82,17 +87,74 @@ end
     @test hasfield(typeof(m.decoder), :factor_regressor)
 end
 
+@testset "Tasic data" begin
+    @info "testing PBMC data loading + VAE model initialization..."
+    using scVI 
+    @info "loading data..."
+    path = joinpath(@__DIR__, "../data")
+    adata = load_tasic(path)
+    @test size(adata.X) == (21, 1500)
+    @test size(adata.obs) == (21, 3)
+    @info "subset to neural cells and receptor + marker genes only..."
+    subset_tasic!(adata)
+    @test size(adata.X) == (15, 80)
+    @test size(adata.var) == (80, 2)
+end
+
+@testset "Highly variable gene utils" begin
+    @info "testing highly variable gene utils: `check_nonnegative_integers`..."
+    using scVI
+    @test scVI.is_nonnegative_integer(0.3) == false 
+    @test scVI.is_nonnegative_integer(-6) == false
+    @test scVI.is_nonnegative_integer(6) == true
+    x = randn(10, 5)
+    @test scVI.check_nonnegative_integers(x) == false
+    x = rand(10, 5)
+    @test scVI.check_nonnegative_integers(x) == false
+    x = rand(-10:10, (10, 5))
+    @test scVI.check_nonnegative_integers(x) == false
+    x = rand(1:10, (10, 5))
+    @test scVI.check_nonnegative_integers(x) == true
+    x = Float32.(x)
+    @test scVI.check_nonnegative_integers(x) == true
+end
+
 @testset "basic scVAE models" begin
     # ZINB distribution
     @info "testing scVAE model training with ZINB distribution..."
     using scVI
     @info "loading data..."
-    adata = load_pbmc()
+    path = joinpath(@__DIR__, "../data")
+    adata = load_tasic(path)
     @info "data loaded, initialising object... "
     library_log_means, library_log_vars = init_library_size(adata) 
+    @info "Using model with wrong number of layers to see if warning is outputted correctly and number of layers adjusted..."
+    m = scVAE(size(adata.X,2);
+        n_layers = 2,
+        n_hidden = [128, 64, 32]
+    )
+    @test m.n_layers == 2
+    @test m.n_hidden == 128 
+    @info "Testing `use_observed_lib_size=false`..."
+    try
+        m = scVAE(size(adata.X,2);
+            use_observed_lib_size=false
+        )
+    catch e
+        @test isa(e, ArgumentError)
+    end
+    @info "Testing wrong gene likelihood specification..."
+    m = scVAE(size(adata.X, 2), 
+        gene_likelihood = :gamma_poisson
+    )
+    @test m.gene_likelihood == :nb
+
+    @info "now trying with correctly specified model..."
     m = scVAE(size(adata.X,2);
             library_log_means=library_log_means,
-            n_latent=2
+            library_log_vars=library_log_vars,
+            n_latent=2, 
+            use_observed_lib_size = false
     )
     print(summary(m))
     training_args = TrainingArgs(
@@ -155,6 +217,6 @@ end
     @test m.gene_likelihood == :bernoulli
     lossval = scVI.loss(m, adata.layers["binarized"]'; kl_weight=1.0f0)
     @test isa(lossval, Float32)
-
 end
+
 
